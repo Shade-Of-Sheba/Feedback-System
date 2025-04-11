67d83ef3f717e8968ee8653e0a7c442c662c0ffd
yeab-B <yeabsirabehailu92@gmail.com>
2025-04-11 12:03:57 +0300

    update the service.js

--- server/server.js
+++ server/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 const feedbackRoutes = require('./routes/feedback');
+
+app.use(express.json());
 app.use('/api/feedback', feedbackRoutes);
+
+mongoose.connect('mongodb://localhost:27017/your-db-name', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('MongoDB connected');
+  app.listen(5000, () => console.log('Server running on port 5000'));
+})
+.catch((err) => console.error(err));
