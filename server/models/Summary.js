745945e52c00788b5c634cf8e19072ef020967b1
tinbit25 <tinbitelias25@gmail.com>
2025-04-10 19:27:32 -0700

    Refactor code to use CommonJS syntax

--- server/models/Summary.js
+++ server/models/Summary.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+
 const summarySchema = new mongoose.Schema({
   source: String,
   summary: String,
   date: { type: Date, default: Date.now },
 });
 
-export default mongoose.model('Summary', summarySchema);
+module.exports = mongoose.model('Summary', summarySchema);
